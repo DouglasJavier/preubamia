7db9289ae637049e6e71b90c8b42de8e0a56b476
Douglas Javier Colque Mullisaca <49354025+DouglasJavier@users.noreply.github.com>
2021-03-17 12:49:38 -0400

    Add files via upload

---
+++ cabecera.inc.php
@@ -0,0 +1,16 @@
+<html>
+	<head>
+		<title>Pagina de prueba</title>
+		<style>
+			body {background-color:E7EDF7;}
+			h1 {color:70016D;}
+			#cabecera {width:640;heigth:200;background-color:535EA2;}
+			.menu {float:left;width:200;heigth:200;background-color:53A255;}
+		</style>
+	</head>
+	<body>
+		<div id="cabecera">
+			<h1>UMSA</h1> 
+			<h1>Facultad de Cs. Puras</h1>
+			<p>Bienvenido a la facultad de ciencias puras y naturales</p>
+		</div>
---
+++ cuerpo.inc.php
@@ -0,0 +1,33 @@
+		<div style="float:left;width:440;heigth:200;background-color:5389A2">
+
+		
+		<table>
+			<tr>
+				<td>
+				Informatica
+				</td>
+				<td>
+				<a href="./quimica/">Quimica</a>
+				</td>
+				<td colspan="2">
+				<a href="fisica/">Fisica</a>
+				</td>
+			</tr>
+			<tr>
+				<td>
+				4000
+				</td>
+				<td>
+				800
+				</td>
+				<td>
+				200
+				</td>
+				<td>
+				Otros
+				</td>
+			</tr>
+		</table>
+		<a href="./quimica/index2.php">Quimica</a>
+		</div>
+		
